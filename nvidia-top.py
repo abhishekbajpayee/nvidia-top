f1fa4e1091b5ac1f8ba1307b4a1a535c05488eac
Abhishek Bajpayee <ab9@Abhisheks-Studio-XPS.(none)>
2014-08-28 18:30:55 -0400

    eliminated referencing of nodes via numbers

--- nvidia-top.py
+++ nvidia-top.py
@@ -5,12 +5,6 @@ import os
 import sys
 import curses as crs
 
-def header(csr):
-    
-    csr.addstr(4, 0, '----------------------------------------------------------------------------------------')
-    csr.addstr(5, 0, '%3s' % '#' + '%25s' % 'Name' + '%20s' % 'Mem. Use' + '%15s' % '% Mem. Use' + '%15s' % 'Pow. Use' + '%10s' % 'Temp.')
-    csr.addstr(6, 0, '----------------------------------------------------------------------------------------')
-
 def display_info(root, scr):
 
     gpunum = 0
@@ -18,29 +12,26 @@ def display_info(root, scr):
     for gpu in root.findall('gpu'):
         
         # Name
-        name = gpu.findall('product_name')
-        name =  name[0].text
+        name = gpu.find('product_name').text
         
         # Memory Use
-        mem = gpu.findall('memory_usage')
-        memu = mem[0][1].text
+        mem = gpu.find('memory_usage')
+        memu = mem.find('used').text
         nmemu = memu.split()
-        memt = mem[0][0].text
+        memt = mem.find('total').text
         nmemt = memt.split()
         memuse = memu + '/' + memt
         usage = float(nmemu[0])*100/float(nmemt[0])
         
         # Power Use
-        pow = gpu.findall('power_readings')
-        pow = pow[0]
-        upow = pow[2].text
-        tpow = pow[3].text
+        pow = gpu.find('power_readings')
+        upow = pow.find('power_draw').text
+        tpow = pow.find('power_limit').text
         powuse = upow + '/' + tpow
         
         # Temp
-        temp = gpu.findall('temperature')
-        temp = temp[0]
-        temp = temp[0].text
+        temp = gpu.find('temperature')
+        temp = temp.find('gpu_temp').text
         
         line = '%3d' % gpunum + '%25s' % name + '%20s' % memuse + '%15.2f' % usage + '%15s' % powuse + '%10s' % temp
         scr.addstr(gpunum+8, 0, line)
@@ -54,9 +45,9 @@ width = 88
 data = sp.check_output(['nvidia-smi', '-q', '-x'])
 root = ET.fromstring(data)
 
-timestamp = root[0].text
-driver = root[1].text
-n = int(root[2].text)
+timestamp = root.find('timestamp').text
+driver = root.find('driver_version').text
+n = int(root.find('attached_gpus').text)
 
 scr = crs.initscr()
 scr.keypad(1)
@@ -67,9 +58,9 @@ scr.addstr(2, 0, 'Driver Version:\t' + driver)
 scr.addstr(3, 0, 'Number of GPUs:\t' + repr(n))
 
 # Drawing header
-scr.addstr(4, 0, '-' * width)
-scr.addstr(5, 0, '%3s' % '#' + '%25s' % 'Name' + '%20s' % 'Mem. Use' + '%15s' % '% Mem. Use' + '%15s' % 'Pow. Use' + '%10s' % 'Temp.')
-scr.addstr(6, 0, '-' * width)
+scr.addstr(5, 0, '-' * width)
+scr.addstr(6, 0, '%3s' % '#' + '%25s' % 'Name' + '%20s' % 'Mem. Use' + '%15s' % '% Mem. Use' + '%15s' % 'Pow. Use' + '%10s' % 'Temp.')
+scr.addstr(7, 0, '-' * width)
 
 # 10 tenths = 1 sec <- this is the polling frequency
 crs.halfdelay(10)
@@ -79,7 +70,7 @@ while (1):
     data = sp.check_output(['nvidia-smi', '-q', '-x'])
     root = ET.fromstring(data)
 
-    timestamp = root[0].text
+    timestamp = root.find('timestamp').text
     scr.addstr(1, 0, 'Timestamp:\t' + timestamp)
 
     display_info(root, scr)
